f73618ce2c5c20a651615b1e903afb9f017a662f
AlejandroOchoa97 <167954559+AlejandroOchoa97@users.noreply.github.com>
2025-02-19 17:52:43 -0700

    Llenar Tabla Listo

---
+++ ProyectoPrimerParcial_165647_229842/src/main/java/conexion/Conexion.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexion;
+
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aleja
+ */
+public class Conexion {
+
+    /*Datos del Servidor MYSQL*/
+    String url = "jdbc:mysql://localhost:3306";
+    String nombreBD = "Tareas";
+    String usuario = "root";
+    String contra = "root";
+    String driver = "com.mysql.jdbc.Driver";
+
+    Connection conexion = null;
+
+    // Metodo para establecer la conexion con la BD
+    public Connection conexion() {
+        // Establecemos el intento de la conexion
+        try {
+            // Cargar los driver de la base de datos en tiempo real o dinámico
+            Class.forName(driver);
+            // Establecer la conexion
+            conexion = DriverManager.getConnection(url + "/" + nombreBD, usuario, contra);
+            //Mostrar un mensaje en dado caso que la conexion sea correcta
+            /*JOptionPane.showMessageDialog(null,"Conexión Exitosa");*/
+        } catch (HeadlessException | ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Conexión fallida: " + e);
+        }
+        return conexion;
+    }
+
+    public void desconectar() {
+        try {
+            if (conexion != null && !conexion.isClosed()) {
+                conexion.close();
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+}
